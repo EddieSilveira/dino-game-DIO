4ef7a008b471f43facf3b0a16594a3d759c8b127
Eddie Silveira <eddie_aparecido@hotmail.com>
2021-01-14 19:50:06 -0300

    Jogo do Dinossauro desenvolvido durante a aula de Javascript com Contador de Score extra

---
+++ script.js
@@ -0,0 +1,75 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
+let position = 0;
+let alive = true;
+let count = 0;
+
+function handleKeyUp(event){
+  if(event.keyCode == 32){
+    if(!isJumping){
+      jump();
+    }
+  }
+}
+
+function jump(){  
+  let upInterval = setInterval(() => {
+    if(position >= 150) {
+      clearInterval(upInterval);
+
+      let domInterval = setInterval(() => {
+        if(position <= 0){
+          clearInterval(domInterval);
+        }else{
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+      }, 20);
+    }else{
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
+}
+
+function createCactus(){
+  const cactus = document.createElement('div');
+  let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
+  cactus.classList.add('cactus');
+  cactus.style.left = 1000 + 'px';
+  background.appendChild(cactus);
+
+  let leftInterval = setInterval(() => {
+    if(cactusPosition <= -60){
+      clearInterval(leftInterval);
+      background.removeChild(cactus);
+    }else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
+      //Game over
+      clearInterval(leftInterval);
+      document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>';
+      alive = false;
+      console.log(alive)
+    }else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + 'px';  
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime);
+  const countTimeout = setTimeout(() => countPoints(), 1000);
+  if(count == null || alive == false) {
+    clearTimeout(countTimeout);
+  }
+}
+
+function countPoints(){
+  const countScore = document.querySelector('.count-points');
+  count++;
+  countScore.innerText = count; 
+} 
+
+createCactus();
+document.addEventListener('keyup', handleKeyUp);
